705816ea038d0a29bc32fca45744465271851ace
James Vastbinder <james.vastbinder@gmail.com>
2015-12-16 12:26:42 -0800

    version bump to v1.0.51 for release

--- app/decl.go
+++ app/decl.go
@@ -10,7 +10,7 @@ import (
 const (
 	ConstVersionMajor = 1
 	ConstVersionMinor = 0
-	ConstSprintNumber = 47
+	ConstSprintNumber = 51
 
 	ConstConfigPathGroup      = "general"
 	ConstConfigPathAppGroup   = "general.app"
