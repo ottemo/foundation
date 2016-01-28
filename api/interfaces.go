b5817582035523c848d19eb713589015f1956da0
Adam Knox <adam.w.knox@gmail.com>
2016-01-28 11:02:48 -0500

    cleanup
    * remove verb constants
    * remove RegisterAPI method
    * add some description to wrappedHandler
    * update the it.Handlers logging
    * move rootPageHandler to it's own function

--- api/interfaces.go
+++ api/interfaces.go
@@ -41,7 +41,6 @@ type InterfaceRestService interface {
 	GetName() string
 
 	Run() error
-	RegisterAPI(resource string, operation string, handler FuncAPIHandler) error
 	GET(resource string, handler FuncAPIHandler)
 	PUT(resource string, handler FuncAPIHandler)
 	POST(resource string, handler FuncAPIHandler)
