4e3edc66db08b5bcea48cad654129ff78b7c2718
James Vastbinder <james.vastbinder@gmail.com>
2016-03-07 19:41:45 -0800

    resend order confirmation email
    [#115210557 Finish]

--- app/actors/order/api.go
+++ app/actors/order/api.go
@@ -1,12 +1,19 @@
 package order
 
 import (
+	"strings"
+
 	"github.com/ottemo/foundation/api"
-	"github.com/ottemo/foundation/app/models"
-	"github.com/ottemo/foundation/app/models/order"
-	"github.com/ottemo/foundation/app/models/visitor"
+	"github.com/ottemo/foundation/app"
 	"github.com/ottemo/foundation/env"
 	"github.com/ottemo/foundation/utils"
+
+	"github.com/ottemo/foundation/app/models"
+	"github.com/ottemo/foundation/app/models/checkout"
+	"github.com/ottemo/foundation/app/models/order"
+	"github.com/ottemo/foundation/app/models/visitor"
+
+	"github.com/ottemo/foundation/app/actors/discount/giftcard"
 )
 
 // setupAPI setups package related API endpoint routines
@@ -21,6 +28,7 @@ func setupAPI() error {
 	service.GET("order/:orderID", APIGetOrder)
 	service.PUT("order/:orderID", APIUpdateOrder)
 	service.DELETE("order/:orderID", APIDeleteOrder)
+	service.POST("order/:orderID/sendConfirmation", APISendConfirmation)
 
 	// service.POST("order", APICreateOrder)
 
@@ -78,20 +86,10 @@ func APIListOrders(context api.InterfaceApplicationContext) (interface{}, error)
 //   - order id should be specified in "orderID" argument
 func APIGetOrder(context api.InterfaceApplicationContext) (interface{}, error) {
 
-	// check request context
-	//---------------------
-	orderID := context.GetRequestArgument("orderID")
-	if orderID == "" {
-		return nil, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "723ef443-f974-4455-9be0-a8af13916554", "order id should be specified")
-	}
+	var orderModel order.InterfaceOrder
+	var err error
 
-	orderModel, err := order.LoadOrderByID(orderID)
-	if err != nil {
-		return nil, env.ErrorDispatch(err)
-	}
-
-	// check rights
-	if err := api.ValidateAdminRights(context); err != nil {
+	if orderModel, err = getOrderID(context); err != nil {
 		return nil, env.ErrorDispatch(err)
 	}
 
@@ -110,26 +108,16 @@ func APIGetOrder(context api.InterfaceApplicationContext) (interface{}, error) {
 //   - order id should be specified in "orderID" argument
 func APIUpdateOrder(context api.InterfaceApplicationContext) (interface{}, error) {
 
-	// check request context
-	//---------------------
-	orderID := context.GetRequestArgument("orderID")
-	if orderID == "" {
-		return nil, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "20a08638-e9e6-428b-b70c-a418d7821e4b", "order id should be specified")
+	var orderModel order.InterfaceOrder
+	var err error
+	var orderID string
+
+	if orderModel, err = getOrderID(context); err != nil {
+		return nil, env.ErrorDispatch(err)
 	}
+	orderID = orderModel.GetID()
 
 	requestData, err := api.GetRequestContentAsMap(context)
-	if err != nil {
-		return nil, env.ErrorDispatch(err)
-	}
-
-	// check rights
-	if err := api.ValidateAdminRights(context); err != nil {
-		return nil, env.ErrorDispatch(err)
-	}
-
-	// operation
-	//----------
-	orderModel, err := order.LoadOrderByID(orderID)
 	if err != nil {
 		return nil, env.ErrorDispatch(err)
 	}
@@ -138,6 +126,7 @@ func APIUpdateOrder(context api.InterfaceApplicationContext) (interface{}, error
 		orderModel.Set(attribute, value)
 	}
 
+	// TODO: shouldn't need this
 	orderModel.SetID(orderID)
 	orderModel.Save()
 
@@ -231,4 +220,114 @@ func APIGetVisitOrders(context api.InterfaceApplicationContext) (interface{}, er
 	result, err := orderCollection.List()
 
 	return result, env.ErrorDispatch(err)
+}
+
+// APISendConfirmation will send out an order confirmation email to the visitor specficied in the orderID
+func APISendConfirmation(context api.InterfaceApplicationContext) (interface{}, error) {
+
+	var orderModel order.InterfaceOrder
+	var orderItems []map[string]interface{}
+	var err error
+	var email, emailAddress, timeZone, giftCardSku string
+
+	if orderModel, err = getOrderID(context); err != nil {
+		return "failure", env.ErrorDispatch(err)
+	}
+
+	email = utils.InterfaceToString(env.ConfigGetValue(checkout.ConstConfigPathConfirmationEmail))
+	timeZone = utils.InterfaceToString(env.ConfigGetValue(app.ConstConfigPathStoreTimeZone))
+	giftCardSku = utils.InterfaceToString(env.ConfigGetValue(giftcard.ConstConfigPathGiftCardSKU))
+
+	// create visitor map
+	visitor := make(map[string]interface{})
+	visitor["first_name"] = orderModel.Get("customer_name")
+	visitor["email"] = orderModel.Get("customer_email")
+
+	// create order map
+	order := orderModel.ToHashMap()
+
+	// load order items
+	for _, item := range orderModel.GetItems() {
+		options := make(map[string]interface{})
+
+		for optionName, optionKeys := range item.GetOptions() {
+			optionMap := utils.InterfaceToMap(optionKeys)
+			options[optionName] = optionMap["value"]
+
+			// Giftcard's delivery date
+			if strings.Contains(item.GetSku(), giftCardSku) {
+				if utils.IsAmongStr(optionName, "Date", "Delivery Date", "send_date", "Send Date", "date") {
+					// Localize and format the date
+					giftcardDeliveryDate, _ := utils.MakeTZTime(utils.InterfaceToTime(optionMap["value"]), timeZone)
+					if !utils.IsZeroTime(giftcardDeliveryDate) {
+						//TODO: Should be "Monday Jan 2 15:04 (MST)" but we have a bug
+						options[optionName] = giftcardDeliveryDate.Format("Monday Jan 2 15:04")
+					}
+				}
+			}
+		}
+
+		orderItems = append(orderItems, map[string]interface{}{
+			"name":    item.GetName(),
+			"options": options,
+			"sku":     item.GetSku(),
+			"qty":     item.GetQty(),
+			"price":   item.GetPrice()})
+	}
+
+	// convert date of order creation to store time zone
+	if date, present := order["created_at"]; present {
+		convertedDate, _ := utils.MakeTZTime(utils.InterfaceToTime(date), timeZone)
+		if !utils.IsZeroTime(convertedDate) {
+			order["created_at"] = convertedDate
+		}
+	}
+
+	order["items"] = orderItems
+	order["payment_method_title"] = orderModel.GetPaymentMethod()
+	order["shipping_method_title"] = orderModel.GetShippingMethod()
+
+	customInfo := make(map[string]interface{})
+	customInfo["base_storefront_url"] = utils.InterfaceToString(env.ConfigGetValue(app.ConstConfigPathStorefrontURL))
+
+	confirmationEmail, err := utils.TextTemplate(email, map[string]interface{}{
+		"Order":   order,
+		"Visitor": visitor,
+		"Info":    customInfo,
+	})
+	if err != nil {
+		return "failure", env.ErrorDispatch(err)
+	}
+
+	emailAddress = utils.InterfaceToString(visitor["email"])
+	err = app.SendMail(emailAddress, "Order confirmation", confirmationEmail)
+	if err != nil {
+		return "failure", env.ErrorDispatch(err)
+	}
+
+	return "success", nil
+}
+
+// getOrderID will load the order from the database
+func getOrderID(context api.InterfaceApplicationContext) (order.InterfaceOrder, error) {
+
+	var orderID string
+	var orderModel order.InterfaceOrder
+	var err error
+
+	// check rights
+	if err := api.ValidateAdminRights(context); err != nil {
+		return nil, env.ErrorDispatch(err)
+	}
+
+	// load orderID
+	if orderID = context.GetRequestArgument("orderID"); orderID == "" {
+		return nil, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "fc3011c7-e58c-4433-b9b0-881a7ba005cf", "order id should be specified")
+	}
+
+	if orderModel, err = order.LoadOrderByID(orderID); err != nil {
+		return nil, env.ErrorDispatch(err)
+	}
+
+	return orderModel, nil
 }
