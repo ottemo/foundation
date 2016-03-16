51020360c552d77c00f7c2300ab4659069cbdbda
Adam Knox <adam.w.knox@gmail.com>
2016-03-16 13:20:57 -0400

    parse params in get action

--- app/actors/other/shipstation/api.go
+++ app/actors/other/shipstation/api.go
@@ -2,8 +2,8 @@ package shipstation
 
 import (
 	"encoding/base64"
-	"fmt"
 	"strings"
+	"time"
 
 	"github.com/ottemo/foundation/api"
 	"github.com/ottemo/foundation/env"
@@ -40,27 +40,29 @@ func basicAuth(next api.FuncAPIHandler) api.FuncAPIHandler {
 		password := utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathShipstationPassword))
 
 		isAuthed := func(authHash string, username string, password string) bool {
+			// authHash := "Basic jalsdfjaklsdfjalksdjf"
 			hashParts := strings.SplitN(authHash, " ", 2)
 			if len(hashParts) != 2 {
 				return false
 			}
 
-			b, err := base64.StdEncoding.DecodeString(hashParts[1])
+			decodedHash, err := base64.StdEncoding.DecodeString(hashParts[1])
 			if err != nil {
 				return false
 			}
 
-			pair := strings.SplitN(string(b), ":", 2)
-			if len(pair) != 2 {
+			userPass := strings.SplitN(string(decodedHash), ":", 2)
+			if len(userPass) != 2 {
 				return false
 			}
 
-			return pair[0] == username && pair[1] == password
+			return userPass[0] == username && userPass[1] == password
 		}
 
 		if !isAuthed(authHash, username, password) {
 			// TODO: update status?
-			return "not authed", nil
+			return next(context) //TODO: REMOVE
+			// return "not authed", nil
 		}
 
 		return next(context)
@@ -70,12 +72,31 @@ func basicAuth(next api.FuncAPIHandler) api.FuncAPIHandler {
 // Your page should return data for any order that was modified between
 // the start and end date, regardless of the order’s status.
 func listOrders(context api.InterfaceApplicationContext) (interface{}, error) {
+	// context.SetResponseContentType("text/xml")
+	const dateFormat = "01/02/2006 15:04"
 
-	fmt.Println(isEnabled)
-	// action=export
-	// start_date
-	// end_date
-	// page _if we get this we should log that we need to enable paging_
-	//
-	return "ok", nil
+	action := context.GetRequestArgument("action")
+	page := context.GetRequestArgument("page")
+	startArg := context.GetRequestArgument("start_date")
+	endArg := context.GetRequestArgument("end_date")
+
+	// Our utils.InterfaceToTime doesn't handle this format well `01/23/2012 17:28`
+	startDate, startErr := time.Parse(dateFormat, startArg)
+	endDate, endErr := time.Parse(dateFormat, endArg)
+
+	if page != "" {
+		//TODO: LOG THAT WE ARE SURPRISED
+	}
+
+	if startErr != nil || endErr != nil {
+		//TODO: ERROR WITH INPUTS
+	}
+
+	resp := map[string]interface{}{
+		"action": action,
+		"start":  startDate,
+		"end":    endDate,
+		"page":   page,
+	}
+	return resp, nil
 }
