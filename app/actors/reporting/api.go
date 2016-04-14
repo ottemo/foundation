629e3b4170e097d1b5fc2093cf752dfe766557e8
Adam Knox <adam.w.knox@gmail.com>
2016-04-14 10:15:06 -0400

    customer activity endpoint

--- app/actors/reporting/api.go
+++ app/actors/reporting/api.go
@@ -7,6 +7,7 @@ import (
 	"github.com/ottemo/foundation/env"
 	"github.com/ottemo/foundation/utils"
 	"sort"
+	"time"
 )
 
 // setupAPI setups package related API endpoint routines
@@ -14,18 +15,14 @@ func setupAPI() error {
 
 	// Admin only endpoint
 	service := api.GetRestService()
-	service.GET("reporting/product-performance", listProductPerformance)
+	service.GET("reporting/product-performance", api.IsAdmin(listProductPerformance))
+	service.GET("reporting/customer-activity", api.IsAdmin(listCustomerActivity))
 
 	return nil
 }
 
 // listProductPerformance Handler that returns product performance information by date range
 func listProductPerformance(context api.InterfaceApplicationContext) (interface{}, error) {
-
-	// check rights
-	if err := api.ValidateAdminRights(context); err != nil {
-		return nil, env.ErrorDispatch(err)
-	}
 
 	// Expecting dates in UTC, and adjusted for your timezone
 	// `2006-01-02 15:04`
@@ -42,7 +39,7 @@ func listProductPerformance(context api.InterfaceApplicationContext) (interface{
 		return nil, env.ErrorNew("reporting", 6, "2eb9680c-d9a8-42ce-af63-fd6b0b742d0d", msg)
 	}
 
-    foundOrders := order.GetOrdersCreatedBetween(startDate, endDate)
+	foundOrders := order.GetOrdersCreatedBetween(startDate, endDate)
 	foundOrderIds := getOrderIds(foundOrders)
 	foundOrderItems := order.GetItemsForOrders(foundOrderIds)
 	aggregatedResults := aggregateOrderItems(foundOrderItems)
@@ -50,8 +47,8 @@ func listProductPerformance(context api.InterfaceApplicationContext) (interface{
 	totalSales := getTotalSales(foundOrderItems)
 
 	response := map[string]interface{}{
-		"total_orders":     len(foundOrders),
-		"total_items":      len(foundOrderItems),
+		"total_orders":    len(foundOrders),
+		"total_items":     len(foundOrderItems),
 		"total_sales":     totalSales,
 		"aggregate_items": aggregatedResults,
 	}
@@ -93,7 +90,7 @@ func aggregateOrderItems(oitems []map[string]interface{}) []AggrOrderItems {
 	var results []AggrOrderItems
 	for _, item := range keyedResults {
 		// @TODO: Round money is bad
-		item.GrossSales = utils.RoundPrice(item.GrossSales);
+		item.GrossSales = utils.RoundPrice(item.GrossSales)
 		results = append(results, item)
 	}
 
@@ -109,4 +106,115 @@ func getTotalSales(oitems []map[string]interface{}) float64 {
 	}
 
 	return totalSales
+}
+
+func listCustomerActivity(context api.InterfaceApplicationContext) (interface{}, error) {
+	perfStart := time.Now()
+
+	// Limit results count, not the query
+	limit := utils.InterfaceToInt(context.GetRequestArgument("limit"))
+	if limit == 0 {
+		limit = 20
+	}
+
+	// Expecting dates in UTC, and adjusted for your timezone `2006-01-02 15:04`
+	startDate := utils.InterfaceToTime(context.GetRequestArgument("start_date"))
+	endDate := utils.InterfaceToTime(context.GetRequestArgument("end_date"))
+	hasDateRange := !startDate.IsZero() || !endDate.IsZero()
+
+	// Date range validation
+	if hasDateRange {
+		if startDate.IsZero() || endDate.IsZero() {
+			context.SetResponseStatusBadRequest()
+			msg := "start_date or end_date missing from response, or not formatted in YYYY-MM-DD"
+			return nil, env.ErrorNew("reporting", 6, "3ed77c0d-2c54-4401-9feb-6e1d04b8baef", msg)
+		}
+		if startDate.After(endDate) || startDate.Equal(endDate) {
+			context.SetResponseStatusBadRequest()
+			msg := "the start_date must come before the end_date"
+			return nil, env.ErrorNew("reporting", 6, "2eb9680c-d9a8-42ce-af63-fd6b0b742d0d", msg)
+		}
+	}
+
+	// Fetch orders
+	oModel, _ := order.GetOrderCollectionModel()
+	oModel.ListAddExtraAttribute("created_at")
+	oModel.ListAddExtraAttribute("customer_name")
+	oModel.ListAddExtraAttribute("customer_email")
+	oModel.ListAddExtraAttribute("grand_total")
+	if hasDateRange {
+		oModel.GetDBCollection().AddFilter("created_at", ">=", startDate)
+		oModel.GetDBCollection().AddFilter("created_at", "<", endDate)
+	}
+
+	// This is the lite response StructListItem
+	foundOrders, _ := oModel.List()
+	aggregatedResults := aggregateCustomerActivity(foundOrders)
+	resultCount := len(aggregatedResults)
+	if resultCount > limit {
+		aggregatedResults = aggregatedResults[:limit]
+	}
+
+	response := map[string]interface{}{
+		"aggregate_items": aggregatedResults,
+		"meta": map[string]interface{}{
+			"limit": limit,
+			"count": resultCount,
+		},
+		"perf_ms": time.Now().Sub(perfStart).Seconds() * 1e3, // in milliseconds
+	}
+	return response, nil
+}
+
+func aggregateCustomerActivity(foundOrders []models.StructListItem) []CustomerActivityItem {
+	keyedResults := make(map[string]CustomerActivityItem)
+
+	for _, o := range foundOrders {
+		email := utils.InterfaceToString(o.Extra["customer_email"])
+		createdAt := utils.InterfaceToTime(o.Extra["created_at"])
+		item, ok := keyedResults[email]
+
+		// First time, set some static props
+		if !ok {
+			item.Email = email
+			item.EarliestPurchase = createdAt
+			item.LatestPurchase = createdAt
+		} else {
+			if createdAt.Before(item.EarliestPurchase) {
+				item.EarliestPurchase = createdAt
+			}
+			if createdAt.After(item.LatestPurchase) {
+				item.LatestPurchase = createdAt
+			}
+		}
+
+		// Name might be empty, early records had a bug
+		if item.Name == "" {
+			item.Name = utils.InterfaceToString(o.Extra["customer_name"])
+		}
+
+		// Aggregated props
+		item.TotalSales += utils.InterfaceToFloat64(o.Extra["grand_total"])
+		item.TotalOrders++
+
+		// Save
+		keyedResults[email] = item
+	}
+
+	// map to slice
+	var results CustomerActivity
+	for _, i := range keyedResults {
+		// Add in averaging stat now that aggregation is complete
+		i.AverageSales = i.TotalSales / float64(i.TotalOrders)
+
+		// Round money
+		i.TotalSales = utils.RoundPrice(i.TotalSales)
+		i.AverageSales = utils.RoundPrice(i.AverageSales)
+
+		results = append(results, i)
+	}
+
+	sort.Sort(results)
+
+	return results
 }
--- app/actors/reporting/decl.go
+++ app/actors/reporting/decl.go
@@ -1,5 +1,9 @@
 package reporting
 
+import (
+	"time"
+)
+
 type AggrOrderItems struct {
 	Name       string  `json:"name"`
 	Sku        string  `json:"sku"`
@@ -18,9 +22,37 @@ func (a ByUnitsSold) Swap(i, j int) {
 }
 
 func (a ByUnitsSold) Less(i, j int) bool {
-    if a[i].UnitsSold == a[j].UnitsSold {
-        return a[i].Name < a[j].Name
-    } else {
-        return a[i].UnitsSold > a[j].UnitsSold
-    }
+	if a[i].UnitsSold == a[j].UnitsSold {
+		return a[i].Name < a[j].Name
+	} else {
+		return a[i].UnitsSold > a[j].UnitsSold
+	}
+}
+
+type CustomerActivityItem struct {
+	Email            string    `json:"email"`
+	Name             string    `json:"name"`
+	TotalSales       float64   `json:"total_sales"`
+	TotalOrders      int       `json:"total_orders"`
+	AverageSales     float64   `json:"avg_sales"`
+	EarliestPurchase time.Time `json:"earliest_purchase"`
+	LatestPurchase   time.Time `json:"latest_purchase"`
+}
+
+type CustomerActivity []CustomerActivityItem
+
+func (a CustomerActivity) Len() int {
+	return len(a)
+}
+
+func (a CustomerActivity) Swap(i, j int) {
+	a[i], a[j] = a[j], a[i]
+}
+
+func (a CustomerActivity) Less(i, j int) bool {
+	if a[i].TotalSales == a[j].TotalSales {
+		return a[i].Email < a[j].Email
+	} else {
+		return a[i].TotalSales > a[j].TotalSales
+	}
 }
