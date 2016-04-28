1a3a5bee32b57e8c36873860d01a8e17ebf6a140
Adam Knox <adam.w.knox@gmail.com>
2016-04-28 10:54:42 -0400

    add all payment methods

--- app/actors/payment/stripe/i_payment_method.go
+++ app/actors/payment/stripe/i_payment_method.go
@@ -1,1 +1,50 @@
 package stripe
+
+import (
+	"github.com/ottemo/foundation/app/models/checkout"
+	"github.com/ottemo/foundation/app/models/order"
+	"github.com/ottemo/foundation/env"
+	"github.com/ottemo/foundation/utils"
+)
+
+func (it *Payment) GetCode() string {
+	return ConstPaymentCode
+}
+
+func (it *Payment) GetInternalName() string {
+	return ConstPaymentName
+}
+
+func (it *Payment) GetName() string {
+	return utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathName))
+}
+
+func (it *Payment) GetType() string {
+	// TODO: NOT SURE
+	// checkout.ConstPaymentTypeCreditCard
+	return ""
+}
+
+func (it *Payment) IsAllowed(checkoutInstance checkout.InterfaceCheckout) bool {
+	return utils.InterfaceToBool(env.ConfigGetValue(ConstConfigPathEnabled))
+}
+
+func (it *Payment) IsTokenable(checkoutInstance checkout.InterfaceCheckout) bool {
+	return false
+}
+
+func (it *Payment) Authorize(orderInstance order.InterfaceOrder, paymentInfo map[string]interface{}) (interface{}, error) {
+	return nil, nil
+}
+
+func (it *Payment) Capture(orderInstance order.InterfaceOrder, paymentInfo map[string]interface{}) (interface{}, error) {
+	return nil, nil
+}
+
+func (it *Payment) Refund(orderInstance order.InterfaceOrder, paymentInfo map[string]interface{}) (interface{}, error) {
+	return nil, nil
+}
+
+func (it *Payment) Void(orderInstance order.InterfaceOrder, paymentInfo map[string]interface{}) (interface{}, error) {
+	return nil, nil
+}
