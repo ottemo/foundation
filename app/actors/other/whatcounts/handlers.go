ceba3daccfbc840474048e7dc0daf9621b2bcbe2
James Vastbinder <james.vastbinder@gmail.com>
2016-05-19 19:03:18 -0700

    do not send/receive json
    [#119324713]

--- app/actors/other/whatcounts/handlers.go
+++ app/actors/other/whatcounts/handlers.go
@@ -1,14 +1,11 @@
 package whatcounts
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"strings"
 
-	"github.com/ottemo/foundation/app"
 	"github.com/ottemo/foundation/app/models/order"
 	"github.com/ottemo/foundation/env"
 	"github.com/ottemo/foundation/utils"
@@ -96,41 +93,55 @@ func splitName(name string) (string, string) {
 //Subscribe a user to a MailChimp list when passed:
 //    -- listID string - a MailChimp list id
 //    -- registration Registration - a struct to holded needed data to subscribe to a list
-func Subscribe(registration Registration) error {
+func Subscribe(reg Registration) error {
 
-	var payload []byte
-	var baseURL string
+	var baseURL, realm, apiKey, listID, noConfirmation, forceSubscribe string
 	var err error
 
 	// load the base url
-	if baseURL = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsBaseURL)); baseURL == "" {
-		return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "0221bf42-b4b1-4d44-824f-17a9b4c22d76", "WhatCounts Base URL may not be empty.")
-	}
+	baseURL = "https://secure.whatcounts.com"
+	// if baseURL = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsBaseURL)); baseURL == "" {
+	// 	return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "0221bf42-b4b1-4d44-824f-17a9b4c22d76", "WhatCounts Base URL may not be empty.")
+	// }
 
 	// load the realm
-	if baseURL = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsRealm)); baseURL == "" {
-		return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "cd9e37e9-6322-4534-9cd5-63f806a4f00e", "WhatCounts Base URL may not be empty.")
-	}
+	realm = "karigran"
+	// if realm = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsRealm)); realm == "" {
+	// 	return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "cd9e37e9-6322-4534-9cd5-63f806a4f00e", "WhatCounts Base URL may not be empty.")
+	// }
+
+	// load the API key
+	apiKey = "ompegrab4293"
 
 	// load the whatcounts list id
-	if listID = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsList)); listID == "" {
-		return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "b08e7d67-1b75-4d27-9c46-4234eb47ed90", "Whatcounts List ID may not be empty.")
-	}
+	listID = "5"
+	// if listID = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsList)); listID == "" {
+	// 	return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "b08e7d67-1b75-4d27-9c46-4234eb47ed90", "Whatcounts List ID may not be empty.")
+	// }
 
 	// flag to send a confirmation email
-	noConfirmation = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsNoConfirm))
+	if utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsNoConfirm)) == "1" {
+		noConfirmation = "1"
+	} else {
+		noConfirmation = "0"
+	}
+	fmt.Printf("value of noConfirmation: %v\n", noConfirmation)
 
 	// flag to force subscribe to whatcounts list
-	forceSubscribe = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsForceSub))
-
-	// marshal the json payload
-	if payload, err = json.Marshal(registration); err != nil {
-		return env.ErrorDispatch(err)
+	if utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsForceSub)) == "1" {
+		forceSubscribe = "1"
+	} else {
+		forceSubscribe = "0"
 	}
+	fmt.Printf("value of forceSubscribe: %v\n", forceSubscribe)
 
 	// subscribe to whatcounts
-	if _, err = sendRequest(fmt.Sprintf(baseURL+"/bin/api_web?", listID), payload); err != nil {
-		sendEmail(payload)
+	if _, err = sendRequest(
+		fmt.Sprintf(baseURL + "/bin/api_web?r=" + realm + "&p=" + apiKey + "&c=sub&list_id=" + listID +
+			"&format=99&force_sub=" + forceSubscribe + "&override_confirmation=" + noConfirmation +
+			"&data=email,first,last" + reg.EmailAddress + "," + reg.FirstName + "," + reg.LastName)); err != nil {
+		// sendEmail(payload)
+
 		return env.ErrorDispatch(err)
 	}
 
@@ -156,23 +167,19 @@ func containsItem(order order.InterfaceOrder, triggerList string) bool {
 }
 
 // sendRequest handles the logic for making a json request to the MailChimp server
-func sendRequest(url string, payload []byte) (map[string]interface{}, error) {
+func sendRequest(url string) (string, error) {
 
 	var apiKey string
 
 	// load the api key
 	if apiKey = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsAPIKey)); apiKey == "" {
-		return nil, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "a4bef3b9-e1c2-43ce-b2e2-11f1b08a5682", "MailChimp API key may not be empty.")
+		return "", env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "a4bef3b9-e1c2-43ce-b2e2-11f1b08a5682", "MailChimp API key may not be empty.")
 	}
 
-	buf := bytes.NewBuffer([]byte(payload))
-	request, err := http.NewRequest("POST", url, buf)
+	request, err := http.NewRequest("GET", url, nil)
 	if err != nil {
 		return nil, env.ErrorDispatch(err)
 	}
-
-	request.Header.Set("Content-Type", "application/json")
-	request.SetBasicAuth("key", apiKey)
 
 	client := &http.Client{}
 	response, err := client.Do(request)
@@ -186,55 +193,14 @@ func sendRequest(url string, payload []byte) (map[string]interface{}, error) {
 			status = response.Status
 		}
 
-		return nil, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "1a600d0d-fad4-4927-ad4d-20acb464d7b1", "Unable to subscribe visitor to WhatCounts list, response code returned was "+status)
+		return "", env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "1a600d0d-fad4-4927-ad4d-20acb464d7b1", "Unable to subscribe visitor to WhatCounts list, response code returned was "+status)
 	}
 	defer response.Body.Close()
 
 	responseBody, err := ioutil.ReadAll(response.Body)
 	if err != nil {
-		return nil, env.ErrorDispatch(err)
-	}
-
-	jsonResponse, err := utils.DecodeJSONToStringKeyMap(responseBody)
-	if err != nil {
-		return nil, env.ErrorDispatch(err)
+		return "", env.ErrorDispatch(err)
 	}
 
 	return jsonResponse, nil
-}
-
-// sendEmail will send an email notification to the specificed user in the dashboard,
-// if a subscribe to a MailChimp list fails for any reason
-func sendEmail(payload []byte) error {
-
-	var emailTemplate, supportAddress, subjectLine string
-
-	// populate the email template
-	if emailTemplate = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsEmailTemplate)); emailTemplate == "" {
-		return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "901a60b4-a18f-46a1-992b-094724aa0407", "Whatcounts Email template may not be empty.")
-	}
-
-	// configure the email address to send errors when adding visitor email addresses to MailChimp
-	if supportAddress = utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsSupportAddress)); supportAddress == "" {
-		return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "f47cecad-3584-4bdf-a02a-ed73647e9c39", "Whatcounts Support Email address may not be empty.")
-	}
-
-	// configure the subject of the email for MailChimp errrors
-	if subjectLine := utils.InterfaceToString(env.ConfigGetValue(ConstConfigPathWhatcountsSubjectLine)); subjectLine == "" {
-		return env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "ee67da86-97b1-43ef-aee3-aa5290a0f1e3", "WhatCounts Support Email Subject may not be empty.")
-	}
-
-	var registration map[string]interface{}
-	if err := json.Unmarshal(payload, &registration); err != nil {
-		return env.ErrorDispatch(err)
-	}
-	if email, err := utils.TextTemplate(emailTemplate, registration); err == nil {
-		if err := app.SendMail(supportAddress, subjectLine, email); err != nil {
-			return env.ErrorDispatch(err)
-		}
-	} else {
-		return env.ErrorDispatch(err)
-	}
-
-	return nil
 }
