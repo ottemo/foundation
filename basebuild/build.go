d7ff534d055135f9e9e0ad54588d906ba643eab5
James Vastbinder <james.vastbinder@gmail.com>
2016-06-24 17:06:46 -0700

    update basebuild to remove quickbooks module
    [#120783911]

--- basebuild/build.go
+++ basebuild/build.go
@@ -46,10 +46,9 @@ import (
 	_ "github.com/ottemo/foundation/app/actors/rts"       // Real Time Statistics service
 	_ "github.com/ottemo/foundation/app/actors/seo"       // URL Rewrite support
 
-	_ "github.com/ottemo/foundation/app/actors/other/friendmail" // email friend extension
-	_ "github.com/ottemo/foundation/app/actors/other/grouping"   // products grouping extension
-	_ "github.com/ottemo/foundation/app/actors/other/mailchimp"  // MailChimp integration
-	// _ "github.com/ottemo/foundation/app/actors/other/quickbooks"  // QuickBooks exporting extension
+	_ "github.com/ottemo/foundation/app/actors/other/friendmail"  // email friend extension
+	_ "github.com/ottemo/foundation/app/actors/other/grouping"    // products grouping extension
+	_ "github.com/ottemo/foundation/app/actors/other/mailchimp"   // MailChimp integration
 	_ "github.com/ottemo/foundation/app/actors/other/shipstation" // Shipstation integration
 	_ "github.com/ottemo/foundation/app/actors/other/trustpilot"  // TrustPilot integration
 )
