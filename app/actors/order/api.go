38f02ea2b99d4e2d535b1e392b0b56c17402a677
James Vastbinder <james@ottemo.io>
2016-06-24 17:29:29 -0700

    fix csv export bug (#367)
    * order module eats quickbooks module
    
    [#120783911]
    
    * move csv export to orders mdule
    
    [#120783911]
    
    * rename api endpoint
    
    [#120783911]
    
    * update basebuild to remove quickbooks module
    
    [#120783911]
    
    Conflicts:
    	app/actors/order/api.go

--- app/actors/order/api.go
+++ app/actors/order/api.go
@@ -1,8 +1,17 @@
 package order
 
 import (
+	"encoding/csv"
+	"math"
+	"regexp"
+	"strings"
+	"time"
+
 	"github.com/ottemo/foundation/api"
+	"github.com/ottemo/foundation/app"
+	"github.com/ottemo/foundation/app/actors/discount/giftcard"
 	"github.com/ottemo/foundation/app/models"
+	"github.com/ottemo/foundation/app/models/checkout"
 	"github.com/ottemo/foundation/app/models/order"
 	"github.com/ottemo/foundation/app/models/visitor"
 	"github.com/ottemo/foundation/env"
@@ -17,6 +26,7 @@ func setupAPI() error {
 	// Admin
 	service.GET("orders/attributes", api.IsAdmin(APIListOrderAttributes))
 	service.GET("orders", api.IsAdmin(APIListOrders))
+	service.POST("orders/exportToCSV", api.IsAdmin(APIExportOrders))
 
 	service.GET("order/:orderID", api.IsAdmin(APIGetOrder))
 	service.PUT("order/:orderID", api.IsAdmin(APIUpdateOrder))
@@ -227,4 +237,299 @@ func APIGetVisitOrders(context api.InterfaceApplicationContext) (interface{}, er
 	result, err := orderCollection.List()
 
 	return result, env.ErrorDispatch(err)
+}
+
+// APISendOrderConfirmationEmail will send out an order confirmation email to the visitor specficied in the orderID
+//   - orderID must be passed as a request argument
+func APISendOrderConfirmationEmail(context api.InterfaceApplicationContext) (interface{}, error) {
+
+	// load the email template
+	email := utils.InterfaceToString(env.ConfigGetValue(checkout.ConstConfigPathConfirmationEmail))
+
+	// load the order
+	orderModel, err := getOrder(context)
+	if err != nil {
+		return nil, env.ErrorDispatch(err)
+	}
+
+	// set visitor
+	visitor := make(map[string]interface{})
+	visitor["first_name"] = orderModel.Get("customer_name")
+	visitor["email"] = orderModel.Get("customer_email")
+
+	order := hydrateOrder(orderModel)
+
+	// set store url
+	customInfo := make(map[string]interface{})
+	customInfo["base_storefront_url"] = utils.InterfaceToString(env.ConfigGetValue(app.ConstConfigPathStorefrontURL))
+
+	// build the email
+	confirmationEmail, err := utils.TextTemplate(email, map[string]interface{}{
+		"Order":   order,
+		"Visitor": visitor,
+		"Info":    customInfo,
+	})
+	if err != nil {
+		context.SetResponseStatusInternalServerError()
+		return "failure", env.ErrorDispatch(err)
+	}
+
+	// send the email
+	emailAddress := utils.InterfaceToString(visitor["email"])
+	err = app.SendMail(emailAddress, "Order confirmation", confirmationEmail)
+	if err != nil {
+		context.SetResponseStatusInternalServerError()
+		return "failure", env.ErrorDispatch(err)
+	}
+
+	return "Order confirmation email sent", nil
+}
+
+// getOrder will load the order from the database using the orderID from the application context
+func getOrder(context api.InterfaceApplicationContext) (order.InterfaceOrder, error) {
+
+	orderID, err := getOrderID(context)
+	if err != nil {
+		return nil, env.ErrorDispatch(err)
+	}
+
+	orderModel, err := order.LoadOrderByID(orderID)
+	if err != nil {
+		context.SetResponseStatusBadRequest()
+		return nil, env.ErrorDispatch(err)
+	}
+
+	return orderModel, nil
+}
+
+func getOrderID(context api.InterfaceApplicationContext) (string, error) {
+
+	// load orderID
+	orderID := context.GetRequestArgument("orderID")
+	if orderID == "" {
+		context.SetResponseStatusBadRequest()
+		return orderID, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "fc3011c7-e58c-4433-b9b0-881a7ba005cf", "No order id found on request, orderID should be specified")
+	}
+
+	return orderID, nil
+}
+
+func hydrateOrder(orderModel order.InterfaceOrder) map[string]interface{} {
+
+	var orderItems []map[string]interface{}
+
+	order := orderModel.ToHashMap()
+	timeZone := utils.InterfaceToString(env.ConfigGetValue(app.ConstConfigPathStoreTimeZone))
+	giftCardSku := utils.InterfaceToString(env.ConfigGetValue(giftcard.ConstConfigPathGiftCardSKU))
+
+	// convert date of order creation to store time zone
+	if date, present := order["created_at"]; present {
+		convertedDate, _ := utils.MakeTZTime(utils.InterfaceToTime(date), timeZone)
+		if !utils.IsZeroTime(convertedDate) {
+			order["created_at"] = convertedDate
+		}
+	}
+
+	// load items into the order
+	for _, item := range orderModel.GetItems() {
+		options := make(map[string]interface{})
+
+		for optionName, optionKeys := range item.GetOptions() {
+			optionMap := utils.InterfaceToMap(optionKeys)
+
+			if strings.Contains(item.GetSku(), giftCardSku) {
+
+				// if we have a giftcard date, localize the date
+				options[optionName] = ifGiftcardSetLocalDate(item.GetSku(), timeZone, optionMap)
+			} else {
+
+				// if not a date, just set the option value
+				options[optionName] = optionMap["value"]
+			}
+		}
+
+		orderItems = append(orderItems, map[string]interface{}{
+			"name":    item.GetName(),
+			"options": options,
+			"sku":     item.GetSku(),
+			"qty":     item.GetQty(),
+			"price":   item.GetPrice()})
+	}
+
+	order["items"] = orderItems
+	order["payment_method_title"] = orderModel.GetPaymentMethod()
+	order["shipping_method_title"] = orderModel.GetShippingMethod()
+
+	return order
+}
+
+func ifGiftcardSetLocalDate(optionName, timeZone string, optionMap map[string]interface{}) string {
+
+	// make sure we are looking for a date
+	if utils.IsAmongStr(optionName, "Date", "Delivery Date", "send_date", "Send Date", "date") {
+
+		// localize the date
+		giftcardDeliveryDate, _ := utils.MakeTZTime(utils.InterfaceToTime(optionMap["value"]), timeZone)
+
+		// format the date if not zero
+		if !utils.IsZeroTime(giftcardDeliveryDate) {
+			// TODO: Should be "Monday Jan 2 15:04 (MST)" but we have a bug
+			return giftcardDeliveryDate.Format("Monday Jan 2 15:04")
+		}
+	}
+
+	return utils.InterfaceToString(optionMap["value"])
+}
+
+// APIExportOrders returns a list of orders in CSV format
+//    - returns orders specified in url parameters
+//    - must include at least one order
+func APIExportOrders(context api.InterfaceApplicationContext) (interface{}, error) {
+	var itemCSVRecords [][]string
+	var orders []string
+
+	requestData, err := api.GetRequestContentAsMap(context)
+	if err != nil {
+		context.SetResponseStatusInternalServerError()
+		return nil, env.ErrorDispatch(err)
+	}
+
+	// must have at least one order in request
+	if !utils.KeysInMapAndNotBlank(requestData, "orders") {
+		context.SetResponseStatusBadRequest()
+		return nil, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "f2602f73-7cae-4525-8405-9e470681c20e", "Specifiy a minimum of one order in the orders parameter.")
+	}
+
+	// look up the order items collection
+	orderItemsCollectionModel, err := order.GetOrderItemCollectionModel()
+	if err != nil {
+		context.SetResponseStatusInternalServerError()
+		return nil, env.ErrorDispatch(err)
+	}
+	dbOrderItemsCollection := orderItemsCollectionModel.GetDBCollection()
+
+	// look up the order collection
+	orderCollectionModel, err := order.GetOrderCollectionModel()
+	if err != nil {
+		context.SetResponseStatusInternalServerError()
+		return nil, env.ErrorDispatch(err)
+	}
+	dbOrderCollection := orderCollectionModel.GetDBCollection()
+	dbOrderCollection.AddSort("created_at", false)
+
+	// load orders based on order IDs passed
+	orders = utils.InterfaceToStringArray(utils.InterfaceToArray(requestData["orders"]))
+	if orders != nil && len(orders) > 0 && !utils.IsInListStr("all", orders) {
+		dbOrderCollection.AddFilter("_id", "in", orders)
+	}
+
+	ordersRecords, err := dbOrderCollection.Load()
+	if err != nil {
+		context.SetResponseStatusInternalServerError()
+		return nil, env.ErrorDispatch(err)
+	}
+
+	// error if no records returned from db
+	if len(ordersRecords) == 0 {
+		context.SetResponseStatusBadRequest()
+		return nil, env.ErrorNew(ConstErrorModule, env.ConstErrorLevelAPI, "28eac91b-39ec-4034-b664-4004e940a6d1", "No orders were found.")
+	}
+
+	// build the field names for csv file
+	for _, columnsHeaders := range orderFields {
+		itemCSVRecords = append(itemCSVRecords, columnsHeaders)
+	}
+
+	// Webgility importer bombs out on some characters even if they are properly
+	// escaped in a csv: . , \ / ( )
+	regexCleaner, _ := regexp.Compile(`[.,\/\\()]`)
+
+	for _, orderRecord := range ordersRecords {
+
+		dbOrderItemsCollection.ClearFilters()
+		dbOrderItemsCollection.AddFilter("order_id", "=", orderRecord["_id"])
+
+		orderItemsRecords, err := dbOrderItemsCollection.Load()
+		if err != nil {
+			return nil, env.ErrorDispatch(err)
+		}
+
+		orderRecord := utils.InterfaceToMap(orderRecord)
+
+		// convert discount to an absolute value to fix quickbooks bug
+		discount := utils.InterfaceToFloat64(orderRecord["discount"])
+		discount = math.Abs(discount)
+		orderRecord["discount"] = discount
+		shippingAddress := utils.InterfaceToMap(orderRecord["shipping_address"])
+		billingAddress := utils.InterfaceToMap(orderRecord["billing_address"])
+
+		for orderItemIndex, orderItem := range orderItemsRecords {
+
+			// process an order and convert it to csv format
+			for _, inputValues := range dataSet {
+				var rowData []string
+
+				// build the csv row from the order data set
+				for _, value := range inputValues {
+					cellValue := ""
+
+					switch typedValue := value.(type) {
+					case string:
+						cellValue = typedValue
+						switch {
+						case strings.Index(cellValue, "$") == 0:
+							cellValue = utils.InterfaceToString(orderRecord[strings.Replace(cellValue, "$", "", 1)])
+							break
+
+						case strings.HasPrefix(cellValue, "item."):
+							cellValue = utils.InterfaceToString(orderItem[strings.Replace(cellValue, "item.", "", 1)])
+							break
+
+						case strings.HasPrefix(cellValue, "shipping."):
+							addressKey := strings.Replace(cellValue, "shipping.", "", 1)
+							cellValue = utils.InterfaceToString(shippingAddress[addressKey])
+							cellValue = regexCleaner.ReplaceAllString(cellValue, "")
+							break
+
+						case strings.HasPrefix(cellValue, "billing."):
+							addressKey := strings.Replace(cellValue, "billing.", "", 1)
+							cellValue = utils.InterfaceToString(billingAddress[addressKey])
+							cellValue = regexCleaner.ReplaceAllString(cellValue, "")
+							break
+						}
+						break
+
+					case func(record map[string]interface{}) string:
+
+						cellValue = typedValue(orderRecord)
+						break
+
+					case func(int, map[string]interface{}) string:
+						cellValue = typedValue(orderItemIndex, orderItem)
+						break
+
+					}
+					// append cellValue to row
+					rowData = append(rowData, cellValue)
+				}
+				// add row to table
+				itemCSVRecords = append(itemCSVRecords, rowData)
+			}
+		}
+	}
+
+	// preparing csv writer
+	csvWriter := csv.NewWriter(context.GetResponseWriter())
+
+	exportFilename := "orders_export_" + time.Now().Format(time.RFC3339) + ".csv"
+
+	context.SetResponseContentType("text/csv")
+	context.SetResponseSetting("Content-disposition", "attachment;filename="+exportFilename)
+
+	for _, csvRecord := range itemCSVRecords {
+		csvWriter.Write(csvRecord)
+	}
+	csvWriter.Flush()
+
+	return "CSV created successfully", nil
 }
