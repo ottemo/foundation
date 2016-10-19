858582494558fb9927499d4c3f14417a21221de3
bikrus <bikrus@gmail.com>
2016-10-19 06:47:34 -0700

    +api test initial version

---
+++ app/actors/product/review/review_api_test.go
@@ -0,0 +1,22 @@
+package review_test
+
+import (
+	"testing"
+
+	//"github.com/ottemo/foundation/app/actors/product/review"
+)
+
+func TestReviewAPI(tst *testing.T) {
+
+	// redeclare api in case of admin check
+	//apiCreateProductReview := review.APICreateProductReview
+	//apiCreateProductReview := review.APICreateProductReview
+	//
+	//apiListReviews := review.APIListReviews
+	//apiGetProductRating := review.APIGetProductRating
+	//
+	//apiUpdateProductReview := review.APIUpdateProductReview
+	//
+	//apiDeleteProductReview := review.APIDeleteProductReview
+
+}
