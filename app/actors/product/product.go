b9931df159a65884a0bc6e08bfa77c7b073839f9
Dmitry Glushenkov <dglushenkov@speroteck.com>
2016-10-19 20:07:48 -0400

    fix products selectors configs

--- app/actors/product/product.go
+++ app/actors/product/product.go
@@ -635,7 +635,7 @@ func (it *DefaultProduct) GetAttributesInfo() []models.StructAttributeInfo {
 			IsStatic:   true,
 			Label:      "Related Products",
 			Group:      "General",
-			Editors:    "product_selector",
+			Editors:    "products_selector",
 			Options:    "",
 			Default:    "",
 		},
