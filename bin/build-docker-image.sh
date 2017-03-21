05e4475dbb3e32723d904b3ecd6b3786ba28fc07
root <root@awork.localdomain>
2017-03-21 15:41:42 +0200

    switch to new docker image

---
+++ bin/build-docker-image.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# build and push foundation image to registry
+
+MYDIR=$(cd `dirname ${BASH_SOURCE[0]}` && pwd)
+FOUNDATIONREPO="$MYDIR/.."
+cd $FOUNDATIONREPO
+
+date=$(date +%Y%m%d-%H%M%S)
+branch=$(git branch| awk '{print $2}')
+IMAGE="gcr.io/ottemo-kube/foundation:${branch}-${date}"
+
+echo "build foundation executable with golang:1.6 docker image"
+docker run -v "$FOUNDATIONREPO":/go/src/github.com/ottemo/foundation -w /go/src/github.com/ottemo/foundation -e GOOS=linux -e CGO_ENABLED=0 golang:1.6 bin/make.sh -tags mongo,redis
+if [ $? -ne 0 ]; then
+  echo "error in build foundation executable"
+  exit 2
+fi
+
+echo "build alpine based foundation container"
+docker build -t $IMAGE .
+if [ $? -ne 0 ]; then
+  echo "error in build foundation alpine based container"
+  exit 2
+fi
+
+gcloud docker -- push $IMAGE
+if [ $? -ne 0 ]; then
+  echo "error in push image"
+  exit 2
+fi
---
+++ bin/docker-entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+echo "generate foundation config and start it"
+echo "you have to define at least MONGOHOST and MONGODB"
+echo "MONGOUSER, MONGOPASS, SECURE_COOKIE, SSL_CERT, XDOMAIN, REDIS are optional parametes"
+
+if ! [ -n "$MONGOHOST" ] ; then
+  echo "you have to define MONGOHOST environment parameter"
+  exit 2
+fi
+if ! [ -n "$MONGODB" ] ; then
+  echo "you have to define MONGODB environment parameter"
+  exit 2
+fi
+
+uri="mongodb://"
+if [ -n "$MONGOUSER" ] ; then
+  if [ -n "$MONGOPASS" ] ; then
+    uri="$uri$MONGOUSER:$MONGOPASS@"
+  else
+    uri="$uri$MONGOUSER@"
+  fi
+fi
+uri="$uri$MONGOHOST/$MONGODB"
+
+echo "media.fsmedia.folder=/home/ottemo/media" > ottemo.ini
+echo "mongodb.db=$MONGODB" >> ottemo.ini
+echo "mongodb.uri=$uri" >> ottemo.ini
+
+if [ -n "$SECURE_COOKIE" ] ; then
+  echo "secure_cookie=$SECURE_COOKIE" >> ottemo.ini
+else
+  echo "secure_cookie=false" >> ottemo.ini
+fi
+if [ -n "$SSL_CERT" ] ; then
+  echo "ssl.cert=$SSL_CERT" >> ottemo.ini
+fi
+if [ -n "$XDOMAIN" ] ; then
+  echo "xdomain=$XDOMAIN" >> ottemo.ini
+fi
+if [ -n "$REDISHOST" ] ; then
+  echo "redis.servers=$REDISHOST" >> ottemo.ini
+fi
+
+echo "use follow ottemo.ini config:"
+cat ottemo.ini
+
+./foundation
